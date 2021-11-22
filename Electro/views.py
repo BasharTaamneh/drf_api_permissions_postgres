01c5ab2ea4cac3e9fc20946b22a57b1b6e3d907f
bashar adel taamneh <bashartaamneh55@gmail.com>
2021-11-23 00:56:56 +0200

    edite premitions classes

--- Electro/views.py
+++ Electro/views.py
@@ -1,13 +1,17 @@
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from .permissions import IsAuthorOrReadOnly
 from .models import Electro
 from .serializer import ElectroSerializer
 
 
 class ElectroListView(generics.ListCreateAPIView):
+    permission_classes = (IsAuthenticatedOrReadOnly,)
     queryset = Electro.objects.all()
     serializer_class = ElectroSerializer
 
 
 class ElectroDetailView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthorOrReadOnly,)
     queryset = Electro.objects.all()
     serializer_class = ElectroSerializer
